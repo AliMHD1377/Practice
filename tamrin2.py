e705b75ec0ea4ab3b39b5d892356861aaece3254
Ali MHD <alimohammadi.mhd.1377@gmail.com>
2025-12-27 04:48:20 -0800

    fix sum

--- tamrin2.py
+++ tamrin2.py
@@ -4,8 +4,8 @@ print("Printing current and previous number and their sum in a range(10)")
 previous_num = 0
 
 for i in range(1, 11):
-    sum = i + previous_num
-    print(f"current Number {i} Previous Number {previous_num} sum: {sum}")
+    x_sum = i + previous_num
+    print(f"current Number {i} Previous Number {previous_num} sum: {x_sum}")
 
     # set it to the current number
     previous_num = i
